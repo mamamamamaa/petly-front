d39997985453d3b0c6bb2989afb7058f3efe87b7
liuba36 <inkkli24@gmail.com>
2023-02-07 22:35:32 +0100

    add login finish

--- src/components/LoginForm/LoginForm.styled.js
+++ src/components/LoginForm/LoginForm.styled.js
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
 import { NavLink } from "react-router-dom";
-import { media, colors, fontWeights } from "../Layout/Layout.styled";
+import { media } from "../Layout/Layout.styled";
+import { theme } from "../../utils/theme.jsx";
 import { Field } from "formik";
 
 export const Container = styled.div`
@@ -8,20 +9,20 @@ export const Container = styled.div`
   padding-inline: 0px;
   padding-top: 42px;
   padding-bottom: 0px;
-  background-color: ${colors.background};
+  background-color: ${theme.colors.secondary};
   margin-inline: auto;
   box-sizing: border-box;
   ${media.tablet} {
     width: 608px;
     padding: 60px 80px 40px 80px;
-    background-color: ${colors.white};
+    background-color: ${theme.colors.accentText};
     border-radius: 40px;
     box-shadow: 7px 4px 14px rgba(0, 0, 0, 0.11);
   }
   ${media.desktop} {
     width: 618px;
     padding: 60px 80px;
-    background-color: ${colors.white};
+    background-color: ${theme.colors.accentText};
     border-radius: 40px;
     box-shadow: 7px 4px 14px rgba(0, 0, 0, 0.11);
   }
@@ -30,15 +31,15 @@ export const Container = styled.div`
 export const Title = styled.h1`
   margin-bottom: 40px;
   font-family: 'Manrope';
-  font-weight: ${fontWeights.bold};
+  font-weight: ${theme.fontWeights.bold};
   font-size: 24px;
   line-height: 1.4;
   letter-spacing: 0.04em;
   align-items: center;
   text-align: center;
-  color: ${colors.black};
+  color: ${theme.colors.black};
   ${media.tablet} {
-    font-weight: ${fontWeights.medium};
+    font-weight: ${theme.fontWeights.medium};
     font-size: 36px;
   }
 `;
@@ -56,26 +57,26 @@ export const Input = styled(Field)`
   height: 40px;
   padding-left: 14px;
   font-family: 'Manrope';
-  font-weight: ${fontWeights.regular};
+  font-weight: ${theme.fontWeights.regular};
   font-size: 14px;
   line-height: 1.4;
   align-items: center;
   letter-spacing: 0.04em;
-  color: ${colors.black};
+  color: ${theme.colors.black};
   background: transparent;
   border: 1px solid rgba(245, 146, 86, 0.5);
   border-radius: 40px;
   &::placeholder {
     font-style: normal;
-    font-weight: ${fontWeights.regular};
+    font-weight: ${theme.fontWeights.regular};
     font-size: 14px;
     line-height: 1.36;
     letter-spacing: 0.04em;
-    color: ${colors.gray};
+    color: ${theme.colors.gray};
   }
   :hover,
   :focus {
-    border: 2px solid ${colors.accent};
+    border: 2px solid ${theme.colors.primary};
     outline: 0;
   }
   ${media.tablet} {
@@ -83,14 +84,14 @@ export const Input = styled(Field)`
     padding-left: 32px;
     font-size: 18px;
     line-height: 1.38;
-    background: ${colors.background};
+    background: ${theme.colors.secondary};
     &::placeholder {
       font-style: normal;
-      font-weight: ${fontWeights.regular};
+      font-weight: ${theme.fontWeights.regular};
       font-size: 18px;
       line-height: 1.38;
       letter-spacing: 0.04em;
-      color: ${colors.gray};
+      color: ${theme.colors.gray};
     }
   }
   ${media.desktop} {
@@ -98,14 +99,14 @@ export const Input = styled(Field)`
     padding-left: 32px;
     font-size: 18px;
     line-height: 1.38;
-    background: ${colors.background};
+    background: ${theme.colors.secondary};
     &::placeholder {
       font-style: normal;
-      font-weight: ${fontWeights.regular};
+      font-weight: ${theme.fontWeights.regular};
       font-size: 18px;
       line-height: 1.38;
       letter-spacing: 0.04em;
-      color: ${colors.gray};
+      color: ${theme.colors.gray};
     }
   }
 `;
@@ -123,11 +124,11 @@ export const Button = styled.button`
   margin-bottom: 40px;
   border: none;
   border-radius: 40px;
-  color: ${colors.white};
-  background-color: ${colors.accent};
+  color: ${theme.colors.accentText};
+  background-color: ${theme.colors.primary};
   cursor: pointer;
   font-family: 'Manrope';
-  font-weight: ${fontWeights.medium};
+  font-weight: ${theme.fontWeights.medium};
   font-size: 20px;
   line-height: 1.35px;
   align-items: center;
@@ -135,9 +136,9 @@ export const Button = styled.button`
   letter-spacing: 0.04em;
   :hover,
   :focus {
-    color: ${colors.black};
-    border: 2px solid ${colors.accent};
-    background-color: ${colors.white};
+    color: ${theme.colors.black};
+    border: 2px solid ${theme.colors.primary};
+    background-color: ${theme.colors.accentText};
   }
   ${media.tablet} {
     width: 458px;
@@ -155,15 +156,15 @@ export const Text = styled.div`
   justify-content: center;
   font-family: 'Manrope';
   font-style: normal;
-  font-weight: ${fontWeights.regular};
+  font-weight: ${theme.fontWeights.regular};
   font-size: 12px;
   line-height: 1.33;
   letter-spacing: 0.04em;
-  color: ${colors.gray};
+  color: ${theme.colors.gray};
 `;
 
 export const Link = styled(NavLink)`
-  color: ${colors.blue};
+  color: ${theme.colors.blue};
   text-decoration: none;
 `;
 
