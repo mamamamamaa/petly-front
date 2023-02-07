2586d858b518b74bc608d5035e80ea76272de204
Yulia Semenyuk <semenyukyp@gmail.com>
2023-02-07 21:54:54 +0200

    /

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,23 +1,41 @@
+import { useState } from "react";
 import { useAuth } from "../../redux/hooks";
 import Navigation from "../Navigation/Navigation";
 import AuthMenu from "../AuthMenu/AuthMenu";
 import UserMenu from "../UserMenu/UserMenu";
+import { BurgerIcon } from "../reusable/svg/burgerBtn";
+import { CloseIcon } from "../reusable/svg/close";
+
 import {BarContainer,
         LogoBlack,
         LogoAccent,
         MenuWrpr,
-        UserMenuWrpr
+        MenuContainer,
+        UserMenuWrpr,
+        BurgerBtn
 } from "./AppBar.styled";
 
+
 const AppBar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    console.log(isMenuOpen);
+    const ToggleClickHandler = () => {
+      setIsMenuOpen(!isMenuOpen);
+    }
+
     const { isLoggedIn } = useAuth();
     return (
         <BarContainer>
             <LogoBlack>pe<LogoAccent>t</LogoAccent>ly</LogoBlack>
+                <BurgerBtn onClick={ToggleClickHandler}>
+                    {!isMenuOpen ? <BurgerIcon/> : <CloseIcon/>}
+                </BurgerBtn>
+                <MenuContainer>
                 <MenuWrpr>
                     <Navigation />
                     <UserMenuWrpr>{isLoggedIn ? <UserMenu /> : <AuthMenu />}</UserMenuWrpr>
                 </MenuWrpr>
+                </MenuContainer>
         </BarContainer>
     )
 };
--- src/components/AppBar/AppBar.styled.jsx
+++ src/components/AppBar/AppBar.styled.jsx
@@ -1,7 +1,13 @@
 import styled from '@emotion/styled';
 
+
 export const BarContainer = styled.div`
     display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 16px 20px 42px 20px;
+    margin: 0 auto;
+    outline: 1px solid red;
 `;
 
 export const LogoBlack = styled.span`
@@ -17,8 +23,23 @@ export const LogoAccent = styled.span`
     color: ${props => props.theme.colors.primary};
 `;
 
+export const MenuContainer = styled.div`
+    opacity: 0;
+    visibility: hidden;
+    pointer-events: none;
+    z-index: 100;
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100vw;
+    height: 100vh;
+`;
+
 export const MenuWrpr = styled.div`
-    
+    /* visibility: hidden; */
+    position: relative;
+    display: flex;
+    flex-direction: column;
 `;
 
 export const UserMenuWrpr = styled.div`
@@ -26,12 +47,25 @@ export const UserMenuWrpr = styled.div`
 `;
 
 export const BurgerBtn = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0;
+    width: 40px;
+    height: 40px;
+    background-color: transparent;
+    border: none;
     
 `;
 
-export const MenuIcon = styled.svg`
+// export const MenuIcon = styled.svg`
     
-`;
+
+//         /* */
+//         width: 30px;
+//         height: 20px;
+//         outline: 1px solid red;
+// `;
 
 
 
