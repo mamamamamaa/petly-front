0c01ee27737c1da7f940a4d06c749a682ef30efe
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-02-07 20:22:04 +0200

    title=kitt

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,10 +1,23 @@
 import { SearchTitle, SearchField } from './SearchForm.styled';
+import axios from 'axios';
 
-const SearchForm = ({ children }) => (
-  <SearchTitle>
-    {children}
-    <SearchField placeholder="Search" />
-  </SearchTitle>
-);
-
+const SearchForm = ({ children }) => {
+  const instance = axios.create({
+    baseURL: 'http://localhost:3001',
+  });
+  const getAll = async () => {
+    const { data } = await instance.get(
+      '/api/notices/searchManyTitles?title=kitt'
+    );
+    console.log(data);
+      return data;
+  };
+getAll();
+  return (
+    <SearchTitle>
+      {children}
+      <SearchField placeholder="Search" />
+    </SearchTitle>
+  );
+};
 export default SearchForm;
