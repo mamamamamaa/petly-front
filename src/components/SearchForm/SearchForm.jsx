0c0fea7c71e66d884ff15eb429416e8cdded5925
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-02-08 08:22:48 +0200

    He loves me

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -2,10 +2,9 @@ import { SearchTitle, SearchField } from './SearchForm.styled';
 import axios from 'axios';
 import { useState } from 'react';
 
-
-
 const SearchForm = ({ children }) => {
-   const [firstName, setFirstName] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [option, setOption] = useState('');
   const instance = axios.create({
     baseURL: 'http://localhost:3001',
   });
@@ -14,9 +13,20 @@ const SearchForm = ({ children }) => {
       `/api/notices/searchManyTitles?title=${firstName}`
     );
     console.log(data);
-      return data;
+    return data;
   };
-getAll();
+  const getStrict = async () => {
+    const { data } = await instance.get(
+      `/api/notices/searchOneTitle?title=${firstName}`
+    );
+    console.log(data);
+    return data;
+  };
+  if (firstName !== '') {
+    console.log(option);
+    option ? getStrict() : getAll();
+  }
+
   return (
     <SearchTitle>
       {children}
@@ -25,6 +35,25 @@ getAll();
         value={firstName}
         onChange={e => setFirstName(e.target.value)}
       />
+      Radio buttons:
+      <input
+        type="radio"
+        name="myRadio"
+        value="option"
+        onChange={e => {
+          console.log(e.target.value);
+          return setOption(e.target.value);
+        }}
+        defaultChecked={true}
+      />
+      Show all notices
+      <input
+        type="radio"
+        name="myRadio"
+        value="option"
+        onChange={e => setOption(e.target.value)}
+      />
+      Use strict search:
     </SearchTitle>
   );
 };
