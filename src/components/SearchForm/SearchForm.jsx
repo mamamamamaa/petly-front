0c8be0beeaef4440a65acd91f0cbf23923868d87
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-02-08 07:23:10 +0200

    firstName

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,13 +1,17 @@
 import { SearchTitle, SearchField } from './SearchForm.styled';
 import axios from 'axios';
+import { useState } from 'react';
+
+
 
 const SearchForm = ({ children }) => {
+   const [firstName, setFirstName] = useState('');
   const instance = axios.create({
     baseURL: 'http://localhost:3001',
   });
   const getAll = async () => {
     const { data } = await instance.get(
-      '/api/notices/searchManyTitles?title=kitt'
+      `/api/notices/searchManyTitles?title=${firstName}`
     );
     console.log(data);
       return data;
@@ -16,7 +20,11 @@ getAll();
   return (
     <SearchTitle>
       {children}
-      <SearchField placeholder="Search" />
+      <SearchField
+        placeholder="Search"
+        value={firstName}
+        onChange={e => setFirstName(e.target.value)}
+      />
     </SearchTitle>
   );
 };
