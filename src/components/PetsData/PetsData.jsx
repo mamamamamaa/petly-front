742cf131fac74795461f445e039b644f32491808
Olga Sumaruk <olga_sumaruk@hotmail.com>
2023-02-08 23:52:57 +0200

    open/close modal

--- src/components/PetsData/PetsData.jsx
+++ src/components/PetsData/PetsData.jsx
@@ -1,22 +1,27 @@
+import { useState } from "react";
 import { Modal } from "../Modal/Modal";
 import { ModalAddsPet } from "../ModalAddsPet/ModalAddsPet";
 import { PetsList } from "../PetsList/PetsList";
 
 
 export const PetsData = () => {
-    // запит на бєк і прокидання пропсів для редеру списку карток тварин
-    // при кліку на кнопку відкриття модалки додавання тварини
+    
+    const [showModal, setShowModal] = useState(false);
+    const [pets, setPets] = useState([]);
 
-    const showModal = false;
-    // const showModal = true;
+    // запит на бєк і прокидання пропсів для редеру списку карток тварин
+
+    const handleModal =()=>{
+        setShowModal(prevShowModal=>!prevShowModal)
+    }
     
     return (
     <section>
         <h2>My pets:</h2>
         <p>Add pet</p>
-        <button type="button">+</button>
-        <PetsList/>
-        { showModal && <Modal><ModalAddsPet/></Modal>}
+        <button type="button" onClick={handleModal}>+</button>
+        <PetsList pets={pets}/>
+        { showModal && <Modal onClose={handleModal}><ModalAddsPet/></Modal>}
     </section>
     )
 };
