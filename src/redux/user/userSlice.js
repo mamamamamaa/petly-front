011dc0cd00517d06190c924a74b2f70f56ddb277
Olga Sumaruk <olga_sumaruk@hotmail.com>
2023-02-10 11:49:33 +0200

    add addCases for pending & rejected reaponse

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -1,7 +1,7 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
 import {fetchUserPets, addOwnPet, deleteOneOwnPet} from './operations';
 
-// const extraActions = [fetchUserPets, addOwnPet, deleteOneOwnPet];
+const extraActions = [fetchUserPets, addOwnPet, deleteOneOwnPet];
 
 const initialState = {
   isLoading: false,
@@ -17,8 +17,8 @@ const userSlice = createSlice({
       state.pets = action.payload;
     })
     .addCase(deleteOneOwnPet.fulfilled, (state, action)=>{
-      console.log('state.pets', state.pets);
-      console.log('action.payload', action.payload);
+      // console.log('state.pets', state.pets);
+      // console.log('action.payload', action.payload);
       state.pets = state.pets.filter(pet=>pet._id!==action.payload._id)
     })
     .addCase(addOwnPet.fulfilled, (state, action)=>{
@@ -26,6 +26,27 @@ const userSlice = createSlice({
       // console.log('action.payload', action.payload);
       state.pets = state.pets.push(action.payload);
     })
+    .addMatcher(
+      isAnyOf(...extraActions.map(action => action.pending)),
+      state => {
+        state.error = null;
+        state.isLoading = true
+      }
+    )
+    .addMatcher(
+      isAnyOf(...extraActions.map(action => action.fulfilled)),
+      state => {
+        state.error = null;
+        state.isLoading = false
+      }
+    )
+    .addMatcher(
+      isAnyOf(...extraActions.map(action => action.rejected)),
+      (state, action) => {
+        state.error = action.payload;
+        state.isLoading = false
+      }
+    )
     ,
 });
 
