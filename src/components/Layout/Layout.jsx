d55e6d236d4ec5e4ba6d4af5f6d1316e7e4bff0d
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-02-11 16:26:42 +0200

    rm notices from shared layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,13 +2,11 @@ import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import AppBar from '../AppBar/AppBar';
 import { Container } from 'utils/reusable';
-import NoticesPage from 'pages/NoticesPage';
 export const Layout = () => {
   return (
     <Container>
       <header>
         <AppBar />
-        <NoticesPage />
       </header>
       <Suspense fallback={null}>
         <Outlet />
