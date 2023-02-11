965c8b9620567b5801fb24d5e0df47cc484b2e5c
Olena Nesteruk <pavlena81@gmail.com>
2023-02-11 15:08:16 +0200

    add componentUserCard

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,9 +1,11 @@
 //import { Container } from "../utils/reusable";
-import  UserMenu   from '../components/UserMenu'
+ import UserMenu from '../components/UserMenu'
+import UserCard from '../components/UserCard/UserCard';
 
 const UserPage = () => {
   return <>User page
-    <UserMenu/>     
+    <UserMenu />
+    <UserCard/>
   </>;
 };
 
