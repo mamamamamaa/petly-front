65abb4c3a3bf84066a54f5113af9d87244bea593
ProskurinV <proskyrinv.g@gmail.com>
2023-02-13 02:03:54 +0200

    add

--- src/pages/NoticesPage.styled.jsx
+++ src/pages/NoticesPage.styled.jsx
@@ -52,8 +52,12 @@ export const SearchField = styled.input`
 export const NoticesNavWrapper = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: baseline;
 `;
 export const NoticesNavUl = styled.ul`
+  ${media.mobile} {
+    position: relative;
+  }
   list-style: none;
   display: flex;
   flex-wrap: wrap;
@@ -93,49 +97,52 @@ export const NoticesNavText = styled.p`
   color: ${theme.colors.titleText};
 `;
 export const AddPetToNoticesBtnWrapper = styled.div`
-  width: 176px;
+  ${media.mobile} {
+    position: absolute;
+    left: 220px;
+    top: 455px;
+  }
+  /* width: 176px; */
+  min-width: 50px;
   display: flex;
   align-items: center;
-  justify-content: space-between;
-  ${media.mobile} {
-    display: none;
-  }
-  ${media.desktop} {
-    width: 129px;
+  justify-content: end;
+  cursor: pointer;
+
+  ${media.tablet} {
+    min-width: 129px;
   }
 `;
 export const AddPetToNoticesBtn = styled.button`
-  ${media.mobile} {
-    display: none;
-  }
-  ${media.tabletAndDesktop} {
-    width: 44px;
-    height: 44px;
-    background: #f59256;
-    border-radius: 50%;
-    cursor: pointer;
-    border: 0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+  width: 44px;
+  height: 44px;
+  background: #f59256;
+  border-radius: 50%;
+  cursor: pointer;
+  border: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 export const AddPetToNoticesImg = styled.img`
-  ${media.mobile} {
-    display: none;
-  }
+  display: block;
 `;
+
 export const AddPetToNoticesText = styled.span`
   ${media.mobile} {
     display: none;
   }
   ${media.tabletAndDesktop} {
+    margin-right: 22px;
     display: block;
     font-family: 'Manrope';
     font-style: normal;
     font-weight: 500;
     font-size: 20px;
-    line-height: 27px;
+    line-height: 1.35;
     color: ${theme.colors.titleText};
+  }
+  ${media.desktop} {
+    margin-right: 20px;
   }
 `;
