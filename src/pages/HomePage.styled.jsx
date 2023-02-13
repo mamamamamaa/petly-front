db0558c5c7f2e881241919d33608c0e26db8875f
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-02-13 22:21:21 +0200

    media.desktop vh adjust

--- src/pages/HomePage.styled.jsx
+++ src/pages/HomePage.styled.jsx
@@ -3,7 +3,8 @@ import { theme } from '../utils/theme';
 import { media } from 'components/Layout/Layout.styled';
 import mobileBackground from 'images/mobile/wave-mobile.svg';
 import tabletBackground from 'images/tablet/wave-tablet.svg';
-import desktopBackground from 'images/desktop/wave-desktop.svg';
+import desktopBackgroundLeft from 'images/desktop/wave-group-left-desktop.svg';
+import desktopBackgroundRight from 'images/desktop/wave-group-right-desktop.svg';
 import ladyWithPetMobile1x from 'images/mobile/lady-with-pet-1x.png';
 import ladyWithPetTablet1x from 'images/tablet/lady-with-pet-1x.png';
 import ladyWithPetDesktop1x from 'images/desktop/lady-with-pet-1x.png';
@@ -29,12 +30,16 @@ export const HomePageBgMobileAndTablet = styled.div`
     top: 24vw;
   }
   ${media.desktop} {
-    background-image: url(${desktopBackground});
+    background-image: url(${desktopBackgroundLeft});
     justify-content: space-between;
     padding-right: 16px;
     padding-left: 16px;
     margin-top: 60px;
     justify-content: flex-end;
+    align-items: flex-start;
+    background-position: top 0vw left -2vw;
+    height: 60vh;
+    margin-top: 30vh;
   }
 `;
 export const HomePageWomanWrapper = styled.div`
@@ -55,12 +60,11 @@ export const HomePageWomanImg = styled.img`
     content: url(${ladyWithPetTablet1x});
     display: block;
     max-width: 100%;
-    margin-top: 50vw; //?
+    margin-top: 50vw;  
   }
   ${media.desktop} {
     content: url(${ladyWithPetDesktop1x});
-    margin-top: 0;
-    position: relative;
+    margin-top: -7vh;
   }
 `;
 export const HomePageUnionHeartSvg = styled.img`
