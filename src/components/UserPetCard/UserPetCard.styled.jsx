f699cc6b0a8afdf4418ac6472e1216ffef5710e1
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-02-14 03:09:37 +0200

    initial

---
+++ src/components/UserPetCard/UserPetCard.styled.jsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+
+
+export const PetName = styled.p``;
+export const PetDateOfBirth = styled.span``;
+export const PetBreed = styled.p``;
+export const PetComments = styled.p``;
+export const PetAddBtn = styled.button``;
+export const PetImg = styled.img``;
+export const PetUl = styled.ul``;
+export const PetLi = styled.li``;
