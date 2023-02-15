f63fdb3dbe1cbf9641375729a5da9b38d6dff22a
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-02-15 12:21:20 +0200

    PetsDataAddText mobile v

--- src/components/PetsData/PetsData.jsx
+++ src/components/PetsData/PetsData.jsx
@@ -1,41 +1,58 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import {fetchUserPets } from "../../redux/user/operations";
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUserPets } from '../../redux/user/operations';
 
-import { Modal } from "../Modal/Modal";
-import { ModalAddsPet } from "../ModalAddsPet/ModalAddsPet";
-import { PetsList } from "../PetsList/PetsList";
-import { Section } from "./PetsData.styled";
+import { Modal } from '../Modal/Modal';
+import { ModalAddsPet } from '../ModalAddsPet/ModalAddsPet';
+import { PetsList } from '../PetsList/PetsList';
+import {
+  PetsDataTitle,
+  PetsDataAddText,
+  PetsDataButton,
+  PetsDataSection,
+  PetsDataAddBtnWrapper,
+  PetsDataAddBtnContainer,
+} from './PetsData.styled';
 const selectPets = state => state.user.pets;
-const selectIsLoading = state=> state.isLoading;
+const selectIsLoading = state => state.isLoading;
 const selectError = state => state.error;
 
 export const PetsData = () => {
-    const dispatch = useDispatch();    
-    const isLoading = useSelector(selectIsLoading);
-    const error = useSelector(selectError);
-    const pets = useSelector(selectPets);
-    
-    const [showModal, setShowModal] = useState(false);
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+  const pets = useSelector(selectPets);
 
-    useEffect(() => {
-        dispatch(fetchUserPets());
-    }, [dispatch]);
+  const [showModal, setShowModal] = useState(false);
 
-    const handleModal =()=>{
-        setShowModal(prevShowModal=>!prevShowModal);
-    }
-    
-    return (
-    <Section>
-        <h2>My pets:</h2>
-        <p>Add pet</p>
-        <button type="button" onClick={handleModal}>+</button>
-        {isLoading && <div>Loading...</div>}
-        {error &&  <div>error</div> } 
-        { !error && !isLoading && pets.length===0 && <div>you have no pets yet</div> }
-        { !error && !isLoading && pets.length>0 && <PetsList pets={pets}/>} 
-        { showModal && <Modal onClose={handleModal}><ModalAddsPet onClose={handleModal}/></Modal>}
-    </Section>
-    )
+  useEffect(() => {
+    dispatch(fetchUserPets());
+  }, [dispatch]);
+
+  const handleModal = () => {
+    setShowModal(prevShowModal => !prevShowModal);
+  };
+
+  return (
+    <PetsDataSection>
+      <PetsDataAddBtnContainer>
+        <PetsDataTitle>My pets:</PetsDataTitle>
+        <PetsDataAddBtnWrapper>
+          <PetsDataAddText>Add pet</PetsDataAddText>
+          <PetsDataButton type="button" onClick={handleModal}></PetsDataButton>
+        </PetsDataAddBtnWrapper>
+      </PetsDataAddBtnContainer>
+      {isLoading && <div>Loading...</div>}
+      {error && <div>error</div>}
+      {!error && !isLoading && pets.length === 0 && (
+        <div>you have no pets yet</div>
+      )}
+      {!error && !isLoading && pets.length > 0 && <PetsList pets={pets} />}
+      {showModal && (
+        <Modal onClose={handleModal}>
+          <ModalAddsPet onClose={handleModal} />
+        </Modal>
+      )}
+    </PetsDataSection>
+  );
 };
--- src/components/PetsData/PetsData.styled.jsx
+++ src/components/PetsData/PetsData.styled.jsx
@@ -1,5 +1,40 @@
 import styled from '@emotion/styled';
-
-export const Section = styled.section`
-background-color: #FDF7F2;
+import cross from 'utils/svg/cross.svg';
+export const PetsDataTitle = styled.h2`
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 27px;
+  letter-spacing: 0.04em;
+  color: #000000;
+`;
+export const PetsDataAddBtnContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const PetsDataAddBtnWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const PetsDataAddText = styled.p`
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 27px;
+  color: #111111;
+  margin-right: 15px;
+`;
+export const PetsDataButton = styled.button`
+  border-radius: 50%;
+  border: none;
+  width: 40px;
+  height: 40px;
+  background-color: #f59256;
+  background-image: url(${cross});
+  background-position: center;
+  background-size: 16px 16px;
+  background-repeat: no-repeat;
+  display: inline-block;
+`;
+export const PetsDataSection = styled.div`
+  background-color: #fdf7f2;
 `;
