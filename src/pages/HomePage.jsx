8c6e6031837a2c9d6fd8cdc2510d348d6c3e78ea
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-02-15 15:51:54 +0200

    fallback HomePAge commented

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -27,3 +27,34 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
+
+
+
+
+// fallback kovalenkoandrii version
+// import {
+//   HomePageBg,
+//   HomePageTitle,
+//   HomePageWomanImg,
+//   HomePageWomanWrapper,
+//   HomePageUnionHeartSvg,
+// } from './HomePage.styled';
+
+// const HomePage = () => {
+//   return (
+//     <>
+//       <HomePageTitle>
+//         Take good care of your small pets
+//       </HomePageTitle>
+//       <HomePageBg>
+//         <HomePageWomanWrapper>
+//           <HomePageWomanImg alt="Lady with pet" />
+//           <HomePageUnionHeartSvg alt="Rainbow heart"></HomePageUnionHeartSvg>
+//         </HomePageWomanWrapper>
+//       </HomePageBg>
+//     </>
+//   );
+// };
+
+// export default HomePage;
