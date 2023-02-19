c0880e76ac3f94f42a29a6d37eb4c1c25b600811
Yulia Semenyuk <semenyukyp@gmail.com>
2023-02-19 18:01:02 +0200

    fix notices styles

--- src/components/NoticesContainer/NoticesContainer.styled.jsx
+++ src/components/NoticesContainer/NoticesContainer.styled.jsx
@@ -10,23 +10,41 @@ export const NoticesNavLink = styled(NavLink)``;
 export const NoticesCategoriesList = styled.ul`
   list-style-type: none;
   padding-bottom: 100px;
-  @media (min-width: 768px) {
-    display: flex;
+  ${media.mobile} {
+    display: grid;
+    grid-gap: 32px;
+  }
+  ${media.tablet} {
+    display: grid;
+    grid-gap: 32px;
+    max-width: calc(100vw - 32px);
+    grid-template-columns: repeat(auto-fill, minmax(336px, 1fr));
+    /* display: flex;
     flex-wrap: wrap;
     flex-basis: calc(100% / 2-32px);
+    :nth-last-child(2n) { 
+      margin-left: 32px;
+
+    } */
   }
   ${media.desktop} {
-    flex-basis: calc(100% / 4-16px);
-    padding-bottom: 200px;
+    display: grid;
+    grid-gap: 32px;
+    max-width: calc(100vw - 32px);
+    grid-template-columns: repeat(auto-fill, minmax(288px, 1fr));
+    /* flex-basis: calc(100% / 4-16px);
+    padding-bottom: 200px; */
   }
 `;
 
 export const NoticeCategoryItem = styled.li`
-  margin-bottom: 32px;
+  /* margin-bottom: 32px; */
   background: #ffffff;
   box-shadow: 7px 4px 14px rgba(49, 21, 4, 0.07);
   border-radius: 0px 0px 20px 20px;
-  ${media.tabletAndDesktop} {
-    margin-left: 32px;
+  ${media.tablet} {
+    /* :nth-of-type(2n) {
+      margin-left: 32px;
+    }  */
   }
 `;
