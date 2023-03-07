ec2965d823d66f58f8b4a3ff81ab980728a85e2c
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-03-08 00:00:33 +0200

    preview.length

--- src/components/AddOwnPetStepTwo/AddOwnPetStepTwo.jsx
+++ src/components/AddOwnPetStepTwo/AddOwnPetStepTwo.jsx
@@ -69,6 +69,9 @@ export const AddOwnPetStepTwo = ({ data, next, prev, onClose }) => {
     // restore the preview images when the component mounts
     restorePreview(setPreview);
   }, []);
+  useEffect(() => {
+    preview.length === 0 && setIsFileUpload(false);
+  }, [preview.length]);
   return (
     <Formik>
       <Container>
