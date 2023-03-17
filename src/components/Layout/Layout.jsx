730fb7caa41ed0407d2c52fcca9680f0456b07f6
mamamamamaa <maks.denishchuk@gmail.com>
2023-03-17 18:00:28 +0200

    fix up layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,20 +1,18 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import AppBar from '../AppBar/AppBar';
-import { Container } from '../../utils';
+
 export const Layout = () => {
   return (
     <>
-    {/* // <Container> */}
       <header>
         <AppBar />
       </header>
       <main>
-      <Suspense fallback={null}>
-        <Outlet />
-      </Suspense>
+        <Suspense fallback={null}>
+          <Outlet />
+        </Suspense>
       </main>
-    {/* // </Container> */}
     </>
   );
 };
